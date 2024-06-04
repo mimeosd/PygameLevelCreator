8703a1b3d32ef61834fe1045d5b787235991b81a
mimeosd@yahoo.com <mimeosd@yahoo.com>
2024-06-04 19:42:48 +0200

    commit 4

--- interface_drawer.py
+++ interface_drawer.py
@@ -2,96 +2,22 @@ import pygame
 from file_get_helper import find_png_images
 import constants
 
-class PictureDrawer(pygame.sprite.Sprite):
-    def __init__(self, screen: pygame.Surface) -> None:
-        super().__init__()
-        self.screen = screen
-        self.images_list = []
-        self.rects_list = []
-        self.holder_width = 350
-        self.holder_height = screen.get_height() - 20
-        self.holder_x = 830
-        self.holder_y = 10
-        self.scroll_offset = 0
-        self.scroll_speed = 10
-
-        image_spacing = 30  # Space between images
-
-        x, y = image_spacing, image_spacing
-        max_x = self.holder_width - image_spacing
-
-        for img_path in find_png_images():
-            img = pygame.image.load(img_path).convert_alpha()
-            img = self.scale_image(img)
-            self.images_list.append(img)
-            rect = img.get_rect(topleft=(x, y))
-            self.rects_list.append(rect)
-            print(f"Loaded image: {img_path}, size: {img.get_size()}, position: {rect.topleft}")
-            
-            x += img.get_width() + image_spacing
-            if x > max_x:
-                x = image_spacing
-                y += img.get_height() + image_spacing + 10
-
-        
-    def exporter(self):
-        return self.images_list
-
-    def scale_image(self, img):
-        if img.get_width() > self.holder_width - 10:
-            scale_factor = (self.holder_width - 10) / img.get_width()
-            img = pygame.transform.scale(img, (int(img.get_width() * scale_factor), int(img.get_height() * scale_factor)))
-            print(f"Scaled image to: {img.get_size()}")
-        return img
-
-    def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            for rect in self.rects_list:
-                rect.y += self.scroll_speed
-        if keys[pygame.K_DOWN]:
-            for rect in self.rects_list:
-                rect.y -= self.scroll_speed
-
-
-    def draw(self, surface: pygame.Surface):
-        holder = pygame.Surface((self.holder_width, self.holder_height))
-        holder.fill((255, 122, 61)) # orange
-        holder_rect = holder.get_rect(topleft=(self.holder_x, self.holder_y))
-
-        # Draw images on holder
-        for i in range(len(self.images_list)):
-            img_rect = self.rects_list[i].move(0, self.scroll_offset)
-            
-            gray_rect = pygame.Rect(img_rect.left - 5, img_rect.top - 5, img_rect.width + 10, img_rect.height + 10)
-            
-            holder.blit(self.images_list[i], self.rects_list[i])
-
-        
-        surface.blit(holder, holder_rect.topleft)
-
-
-class ChosenPicture(pygame.sprite.Sprite):
-    def __init__(self, location_x, location_y, image=None) -> None:
-        super().__init__()
-        self.location_x = location_x
-        self.location_y = location_y
-        self.image = image
-        
-            
-
-    def update(self) -> None:
-        if self.image == None:
-            self.image = pygame.Surface((40, 40))
-            self.image.fill((200, 100, 50))
-            self.rect = self.image.get_rect(center=(self.location_x, self.location_y))
-        else:
-            self.image = self.image
-            self.rect = self.image.get_rect(center=(self.location_x, self.location_y))
+def side_panel(surface: pygame.Surface, color: tuple):
+    main_holder = pygame.draw.rect(surface, (215, 0, 0), (constants.WIDTH - 350, 10, 330, constants.HEIGTH - 30), 5) 
+    # height is 780 each block is 30x30px
     
-    def state_setter(self):
-        self.image = None
+    # load images
+    all_cells = []
     
-    def draw(self, surface: pygame.Surface):
-        surface.blit(self.image, self.rect)
-        
+    for row in range(15):
+        for cell in range(6):
+            cell = pygame.draw.rect(surface, (215, 0, 0), (850 + cell * 55, row * 55 + 10, 55, 55), 5)
+            all_cells.append([cell.topleft[0], cell.topleft[1], 55, 55])
+
+    all_images = find_png_images()
+    for _index, _image in enumerate(all_images):
+        img = pygame.image.load(_image)
+        img_rect = img.get_rect()
+        print(img_rect.width)
+        img_rect.topleft = (all_cells[_index][0], all_cells[_index][1])
+        surface.blit(img, img_rect)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import constants
-from interface_drawer import PictureDrawer, ChosenPicture
+from interface_drawer import side_panel
 
 
 pygame.init()
@@ -11,14 +11,6 @@ pygame.display.set_caption("Level creator")
 
 clock = pygame.time.Clock()
 
-pd = PictureDrawer(screen)
-pd_group = pygame.sprite.Group()
-pd_group.add(pd)
-
-chosen_picture_square = ChosenPicture(constants.WIDTH // 2 - 20, 40, pd.exporter()[7])
-chosen_picture_square_group = pygame.sprite.Group()
-chosen_picture_square_group.add(chosen_picture_square)
-
 running = True
 while running:
     pressed = pygame.mouse.get_pressed()
@@ -26,25 +18,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if pressed[1]: #lmb
-            pass # set chosen image to be drawn
-        if pressed[2]:
-            chosen_picture_square.state_setter()
 
 
-    for p in pd_group:
-        p.update()
-        p.draw(screen)
-    
-    for cp in chosen_picture_square_group:
-        cp.update()
-        cp.draw(screen)
 
-
-    # TODO draw grid placement
-    # TODO solve picking image
-
-
+    side_panel(screen, (0, 0, 0))
     
     pygame.display.update()
     clock.tick(60)
